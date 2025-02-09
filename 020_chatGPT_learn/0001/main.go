dc3b8390ecc6fc495fca17f3a5ee1ccec6de1e7b
Rybalka Andrey Anatolyevich <anrybalka@o3.ru>
2025-02-09 21:08:19 +0500

    upd

---
+++ 020_chatGPT_learn/0001/main.go
@@ -0,0 +1,18 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// )
+
+// func main() {
+// 	fmt.Println("Как тебя зовут?")
+
+// 	var userName string
+// 	if _, err := fmt.Scanln(&userName); err != nil {
+// 		log.Fatalf("[ERROR]: %s", err)
+// 	} else {
+// 		fmt.Printf("\nПривет %v", userName)
+// 	}
+
+// }
